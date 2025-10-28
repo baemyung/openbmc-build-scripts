2588d5c386bed5f3fb2704bd0c2501d3bd035376
Myung Bae <myungbae@us.ibm.com>
2025-10-28 14:39:32 -0500

    Add an option to run cppcheck only
    This adds an option to run cppcheck only.
    
    Signed-off-by: Myung Bae <myungbae@us.ibm.com>

---
+++ scripts/run-cppcheck.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# Test for cppcheck filter
+#
+set -uo pipefail
+
+args=$*
+
+if [ -z "${UNIT_TEST_PKG}" ]
+then
+    echo "UNIT_TEST_PKG is not set"
+    exit 1
+fi
+
+if [ -z "${WORKSPACE}" ]
+then
+	WORKSPACE=.
+fi
+
+printenv >  x1.env.out
+echo "run-cppcheck: ARGS=$args"  | tee x2.chk.out
+
+#cppcheck $args 2> x3.err.chk.out | tee x3.chk.out
+
+## Note: Additional necessary opts
+CPPCHK_DEF_OPTS=-D"__cppcheck__=1"
+
+cppcheck ${CPPCHK_DEF_OPTS}  --template="{file}:{line}:{column}: {severity}: {message}" $args  2> x3.err.chk.out | tee x3.chk.out
+
+LOGOUTPUT=${WORKSPACE}/cppchk.${UNIT_TEST_PKG}.error.out
+echo "LOGOUTPUT=$LOGOUTPUT"
+
+FAILED=0
+
+echo "TEST:" > ${LOGOUTPUT}
+while read -r filelinecol severity remaining; do
+    #echo "Filename:Line:Column: $filelinecol" >> ${LOGOUTPUT}
+    #echo "severity: $severity" >> ${LOGOUTPUT}
+    #echo "remaining: $remaining" >> ${LOGOUTPUT}
+    #echo "---" >> ${LOGOUTPUT}
+    if [[ "$filelinecol" == "/"* ]]
+    then
+        continue
+    fi
+    if [[ "$severity" == "warning:" || "$severity" == "error:" ]]
+    then
+        echo "Filename:Line:Column: $filelinecol" >> ${LOGOUTPUT}
+        echo "severity: $severity" >> ${LOGOUTPUT}
+        echo "remaining: $remaining" >> ${LOGOUTPUT}
+        echo "---" >> ${LOGOUTPUT}
+        FAILED=1
+    fi
+done < x3.err.chk.out
+
+if [[ $FAILED -ne 0 ]]
+then
+	echo "cppcheck failed"
+	cat ${LOGOUTPUT}
+	exit 1
+fi
